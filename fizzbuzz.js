3504c3ea1f350b12b80279f02d1f6ae1a9c5be91
Amelia Blackwell <amelia.blackwell@softwire.com>
2023-07-04 09:27:31 +0100

    First attempt at part 1

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,7 +2,19 @@
 function fizzbuzz() {
     console.log("Hello, World!");
 
-    // Put your code here...
+    let n = 0;
+    while (n < 100) {
+        n++;
+        if (n % 3 == 0 && n % 5 == 0) {
+            console.log("FizzBuzz");
+        } else if (n % 3 == 0) {
+            console.log("Fizz");
+        } else if (n % 5 == 0) {
+            console.log("Buzz");
+        } else {
+            console.log(n)
+        }
+    }
 
 }
 
