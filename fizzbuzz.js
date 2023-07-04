a3ce2f32b2532d6521ce954500937e673ca8b907
Amelia Blackwell <amelia.blackwell@softwire.com>
2023-07-04 09:58:58 +0100

    Added Fezz for multiples of 13

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,17 +3,22 @@ function fizzbuzz() {
     console.log("Hello, World!");
 
     let n = 0;
-    while (n < 100) {
+    while (n < 200) {
         n++;
         let result = "";
         if (n % 3 == 0) {
             result += "Fizz";
+        } if (n % 13 == 0) {
+            result += "Fezz";
         } if (n % 5 == 0) {
             result += "Buzz";
         } if (n % 7 == 0) {
             result += "Bang";
         } if (n % 11 == 0) {
-            result = "Bong";
+            result = "";
+            if (n % 13 == 0) {
+                result += "Fezz";
+            } result += "Bong";
         }
         if (result != "") {
             console.log(result);
